ff1d5801ae0fc337fcf3c665282d554bf3a5c033
noahhallows <noah@quackmail.com.au>
2024-11-09 22:04:34 +1100

    trying fix autocomplete + syntax highlighting

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -16,9 +16,9 @@ return {
         local g = vim.g
 
         g.ale_ruby_rubocop_auto_correct_all = 1
-
+        vim.g.ale_completion_enabled = 1
         g.ale_linters = {
-            ruby = {'rubocop', 'ruby'},
+            cs = {'OmniSharp'},
             lua = {'lua_language_server'}
         }
     end,
